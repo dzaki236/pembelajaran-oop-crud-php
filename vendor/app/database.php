5b719f4fd4515e471fbdb06ba25b1595cd9984ca
dzaki236 <dzakiahnafz@gmail.com>
2021-07-31 13:09:37 +0700

    test

--- vendor/app/database.php
+++ vendor/app/database.php
@@ -1,16 +1,16 @@
 <?php
-// namespace app;
-// use PDO;
-// class Database{
-//     public $host,$username,$password,$database,$port;
-// public function __construct()
-// {
-//     $this->host = 'localhost';
-//     $this->username = 'root';
-//     $this->password='abcd5dasar';
-//     $this->db_name = 'warga';
-//     $this->port = '3306';
-//     $this->db = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
-// }
-// }
+namespace app;
+use PDO;
+class Database{
+    public $host,$username,$password,$database,$port;
+public function __construct()
+{
+    $this->host = 'localhost';
+    $this->username = 'root';
+    $this->password='abcd5dasar';
+    $this->db_name = 'warga';
+    $this->port = '3306';
+    $this->db = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+}
+}
 ?>
