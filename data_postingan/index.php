1d7c160a13957f6b5334220c62e0edce4fc7980f
dzaki236 <dzakiahnafz@gmail.com>
2021-08-06 09:06:42 +0700

    membuat contoh(dummy) dan membuat file untuk hash password

---
+++ data_postingan/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Blank</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="./components/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="./components/vendor/fontawesome-free/css/fontawesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <style>
+        img {
+            width: 100%;
+            /* max-height: 100%; */
+            /* max-width: 100%; */
+        }
+    </style>
+    <!-- Custom styles for this template-->
+    <link href="./components/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-lg-4">
+                <!-- Dropdown Card Example -->
+                <div class="card shadow mb-4">
+                    <!-- Card Header - Dropdown -->
+                    <div class="card-body py-3 d-flex flex-row align-items-center justify-content-between">
+                        <img src="https://www.wandah.org/images/post/tutorial/wandah-org-membuat-game-rpg-html5-js-2.jpg" class="img-fluid" alt="...">
+                    </div>
+                    <!-- Card Body -->
+                    <div class="card-body">
+                        Dropdown menus can be placed in the card header in order to extend the functionality
+                        of a basic card. In this dropdown card example, the Font Awesome vertical ellipsis
+                        icon in the card header can be clicked on in order to toggle a dropdown menu.
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <!-- Dropdown Card Example -->
+                <div class="card shadow mb-4">
+                    <!-- Card Header - Dropdown -->
+                    <div class="card-body py-3 d-flex flex-row align-items-center justify-content-between">
+                        <img src="https://mk0omfourop3d6at17y.kinstacdn.com/wp-content/uploads/grapes-2000.jpg" class="img-fluid" alt="...">
+                    </div>
+                    <!-- Card Body -->
+                    <div class="card-body">
+                        Dropdown menus can be placed in the card header in order to extend the functionality
+                        of a basic card. In this dropdown card example, the Font Awesome vertical ellipsis
+                        icon in the card header can be clicked on in order to toggle a dropdown menu.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Bootstrap core JavaScript-->
+        <script src="./components/vendor/jquery/jquery.min.js"></script>
+        <script src="./components/vendor/fontawesome-free//js/all.min.js"></script>
+        <script src="./components/vendor/fontawesome-free/attribution.js"></script>
+        <script src="./components/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Core plugin JavaScript-->
+        <script src="./components/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+        <!-- Custom scripts for all pages-->
+        <script src="./components/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
